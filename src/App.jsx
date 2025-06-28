55029b76e68825b87a515765d1c2d4bae8df3cb7
Nate Simonsen <natejsimonsen@gmail.com>
2025-06-27 21:18:40 -0700

    add basepath per mode

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { TextureBG } from "./components/TextureBG";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={import.meta.env.MODE === "production" ? "sbcws" : ""}>
       <div className="relative w-full min-h-screen">
         <TextureBG />
         <div className="relative z-10 flex flex-col min-h-screen">
